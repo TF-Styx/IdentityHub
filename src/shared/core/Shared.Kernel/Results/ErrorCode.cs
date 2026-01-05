12d16d796f22176973d73cbb5d15bae595cc4fac
texno stalin <texno.stalin@gmail.com>
2026-01-05 15:52:42 +0300

    feat: инициализация проекта с базовыми сущностями

---
+++ src/shared/core/Shared.Kernel/Results/ErrorCode.cs
@@ -0,0 +1,16 @@
+﻿namespace Shared.Kernel.Results
+{
+    public enum ErrorCode
+    {
+        None = 0,
+        Create = 1,
+        Delete = 2,
+        Update = 3,
+        NotFound = 4,
+        InvalidPassword = 5,
+        Server = 6,
+        Conflict = 7,
+        Save = 8,
+        Unauthorized = 9,
+    }
+}
