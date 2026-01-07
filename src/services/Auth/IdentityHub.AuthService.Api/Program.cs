d20525625208af15e441bc7167fd67dcf8180905
texno stalin <texno.stalin@gmail.com>
2026-01-07 13:03:11 +0300

    feat: реализовать доменную модель и миграцию базы данных
    - Добавлена доменная модель
    - Создана миграция базы данных

--- src/services/Auth/IdentityHub.AuthService.Api/Program.cs
+++ src/services/Auth/IdentityHub.AuthService.Api/Program.cs
@@ -1,3 +1,4 @@
+using IdentityHub.AuthService.Infrastructure.Ioc;
 
 namespace IdentityHub.AuthService.Api
 {
@@ -7,15 +8,13 @@ namespace IdentityHub.AuthService.Api
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+
+            builder.Services.AddInfrastructure(builder.Configuration);
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
---
+++ src/services/Auth/IdentityHub.AuthService.Infrastructure/Ioc/InfrastructureDi.cs
@@ -0,0 +1,17 @@
+﻿using IdentityHub.AuthService.Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityHub.AuthService.Infrastructure.Ioc
+{
+    public static class InfrastructureDi
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AuthContext>(options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
+    }
+}
