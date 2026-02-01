40c2d51dfbfcad27651a5456d56ae7bbd129adec
texno stalin <texno.stalin@gmail.com>
2026-02-01 17:39:06 +0300

    (feat): Реализовать http-client для взаимодействия с `IdentityService`

---
+++ src/services/Auth/IdentityHub.AuthService.Application/Services/IIdentityServiceClient.cs
@@ -0,0 +1,11 @@
+﻿using Shared.Contracts.Response.User;
+using Shared.Kernel.Primitives;
+
+namespace IdentityHub.AuthService.Application.Services
+{
+    public interface IIdentityServiceClient
+    {
+        Task<Maybe<UserResponse>> GetUserByIdAsync(string userId, CancellationToken cancellationToken = default);
+        Task<Maybe<UserResponse>> GetUserByLoginAsync(string userId, CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/services/Auth/IdentityHub.AuthService.Infrastructure/Services/IdentityServiceClient.cs
@@ -0,0 +1,40 @@
+﻿using IdentityHub.AuthService.Application.Services;
+using Shared.Contracts.Response.User;
+using Shared.Kernel.Primitives;
+using System.Net.Http.Json;
+
+namespace IdentityHub.AuthService.Infrastructure.Services
+{
+    public sealed class IdentityServiceClient(HttpClient httpClient) : IIdentityServiceClient
+    {
+        private readonly HttpClient _httpClient = httpClient;
+
+        public async Task<Maybe<UserResponse>> GetUserByIdAsync(string userId, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var response = await _httpClient.GetFromJsonAsync<UserResponse>($"internal/api/users/by-id/{userId}", cancellationToken);
+
+                return response;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public async Task<Maybe<UserResponse>> GetUserByLoginAsync(string userId, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var response = await _httpClient.GetFromJsonAsync<UserResponse>($"internal/api/users/y-login/{userId}", cancellationToken);
+
+                return response;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
