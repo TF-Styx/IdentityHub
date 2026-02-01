204f1c837e3b195693551ee76e57809ded3751a2
texno stalin <texno.stalin@gmail.com>
2026-02-01 17:26:59 +0300

    (feat): Реализовать дешифратор ассиметричного шифрования
    Созадана абстракция `IVerifierProtector` в `Shared.Security`
    Создана реализация RSA `RSADecryptor` в `AuthService`

---
+++ src/services/Auth/IdentityHub.AuthService.Application/Services/Protections/RSADecryptor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Shared.Security.Encoders;
+using System.Security.Cryptography;
+
+namespace IdentityHub.AuthService.Application.Services.Protections
+{
+    public sealed class RSADecryptor(IConfiguration configuration) : IVerifierProtector
+    {
+        private readonly string _privateKey = configuration["Security:RSA:PrivateKey"] ?? throw new InvalidOperationException("Приватный ключ не сконфигурирован.");
+
+        public string Protect(string verifier)
+        {
+            throw new NotSupportedException("Данный сервис не обладает функционалом шифрования!");
+        }
+
+        public string Unprotect(string protectedVerifier)
+        {
+            using var rsa = RSA.Create();
+
+            rsa.ImportRSAPrivateKey(Convert.FromBase64String(_privateKey), out _);
+
+            var encryptedData = Convert.FromBase64String(protectedVerifier);
+            var decryptedData = rsa.Decrypt(encryptedData, RSAEncryptionPadding.OaepSHA256);
+
+            return Convert.ToBase64String(decryptedData);
+        }
+    }
+}
---
+++ src/shared/security/Shared.Security/Encoders/IVerifierProtector.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Security.Encoders
+{
+    public interface IVerifierProtector
+    {
+        string Protect(string verifier);
+        string Unprotect(string protectedVerifier);
+    }
+}
