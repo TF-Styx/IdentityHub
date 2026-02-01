a44af8ecbdfe1a55a9779b6b6be27e89c10b0e87
texno stalin <texno.stalin@gmail.com>
2026-02-01 16:33:17 +0300

    (feat): Реализовать кэширующий сервис
    Реализован сервис для взаимодействия с Redis.

--- src/services/Auth/IdentityHub.AuthService.Infrastructure/Ioc/InfrastructureDi.cs
+++ src/services/Auth/IdentityHub.AuthService.Infrastructure/Ioc/InfrastructureDi.cs
@@ -1,7 +1,10 @@
-﻿using IdentityHub.AuthService.Infrastructure.Persistence.Contexts;
+﻿using IdentityHub.AuthService.Application.Abstracts;
+using IdentityHub.AuthService.Infrastructure.Persistence.Contexts;
+using IdentityHub.AuthService.Infrastructure.Redis;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
 
 namespace IdentityHub.AuthService.Infrastructure.Ioc
 {
@@ -10,6 +13,22 @@ namespace IdentityHub.AuthService.Infrastructure.Ioc
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<AuthContext>(options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<AuthContext>());
+
+            services.Configure<RedisOptions>(options => configuration.GetSection(RedisOptions.SectionName));
+            services.AddSingleton<IConnectionMultiplexer>(provider =>
+            {
+                string configurationString = configuration["Redis:ConnectionString"]!;
+
+                var config = ConfigurationOptions.Parse(configurationString);
+
+                config.AbortOnConnectFail = false;
+                config.ConnectRetry = 3;
+                config.ConnectTimeout = 5000;
+
+                return ConnectionMultiplexer.Connect(config);
+            });
 
             return services;
         }
---
+++ src/services/Auth/IdentityHub.AuthService.Infrastructure/Redis/RedisOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace IdentityHub.AuthService.Infrastructure.Redis
+{
+    public sealed record RedisOptions
+    {
+        public const string SectionName = "Redis";
+        public string ConnectionString { get; set; }
+        public int Database { get; set; } = 0;
+    }
+}
