a384588411a4df6ee621668cd07c198403f5faa1
texno stalin <texno.stalin@gmail.com>
2026-02-12 12:56:07 +0300

    feat: Реализована общая библиотека с абстракциями и вспомогательными классами

---
+++ dotnet/src/shared/core/Shared.Kernel/Results/ErrorCode.cs
@@ -0,0 +1,19 @@
+﻿namespace Shared.Kernel.Results
+{
+    public enum ErrorCode
+    {
+        None = 0,
+        Create = 1,
+        Delete = 2,
+        Update = 3,
+        NotFound = 4,
+        InvalidPassword = 5,
+        Server = 6,
+        Conflict = 7,
+        Save = 8,
+        Unauthorized = 9,
+        Validation = 10,
+        ValueObject = 11,
+        Redis = 12
+    }
+}
